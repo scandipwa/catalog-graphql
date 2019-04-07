343ce5db6b8d7682fa8617745fc7fc38aa7b7c76
Ilja Lapkovskis <ilja@scandi.com>
2019-04-07 14:52:26 +0300

    Add path resolver

---
+++ src/Model/Resolver/Product/ProductImage/Path.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ScandiPWA_CatalogGraphQl
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_CatalogGraphQl
+ * @author      Ilja Lapkovskis <info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+namespace ScandiPWA\CatalogGraphQl\Model\Resolver\Product\ProductImage;
+
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class Path implements ResolverInterface
+{
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    )
+    {
+        if (!isset($value['image_type'])) {
+            throw new LocalizedException(__('"image_type" value should be specified'));
+        }
+    
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+    
+        $product = $value['model'];
+        
+        return $product->getData($value['image_type']);
+    }
+}
