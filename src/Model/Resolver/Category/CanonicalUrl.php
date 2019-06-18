99023c470e9414343c136ad40975798a58454e75
Ilja Lapkovskis <ilja@scandi.com>
2019-06-18 10:41:37 +0300

    Update phpdoc header

--- src/Model/Resolver/Category/CanonicalUrl.php
+++ src/Model/Resolver/Category/CanonicalUrl.php
@@ -5,8 +5,10 @@
  * @category    ScandiPWA
  * @package     ScandiPWA_CatalogGraphQl
  * @author      Kriss Andrejevs <info@scandiweb.com>
- * @copyright   Copyright (c) 2018 Scandiweb, Ltd (https://scandiweb.com)
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ * @license     OSL-3.0
  */
+
 declare(strict_types=1);
 
 namespace ScandiPWA\CatalogGraphQl\Model\Resolver\Category;
