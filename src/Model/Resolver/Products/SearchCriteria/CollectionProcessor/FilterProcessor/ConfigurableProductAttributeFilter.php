4e67a5dea7f61533cdbd8083ff34d7c1e5783415
Alfreds Genkins <alfreds@scandiweb.com>
2019-06-19 19:12:57 +0300

    Implemented filters based only on SQL.

--- src/Model/Resolver/Products/SearchCriteria/CollectionProcessor/FilterProcessor/ConfigurableProductAttributeFilter.php
+++ src/Model/Resolver/Products/SearchCriteria/CollectionProcessor/FilterProcessor/ConfigurableProductAttributeFilter.php
@@ -17,6 +17,7 @@ use Magento\Framework\Data\Collection\AbstractDb;
 use Magento\Framework\Api\Filter;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
+use Magento\Framework\Registry;
 
 /**
  * Category filter allows to filter products collection using custom defined filters from search criteria.
@@ -25,51 +26,52 @@ class ConfigurableProductAttributeFilter implements CustomFilterInterface
 {
     protected $configurable;
     protected $collectionFactory;
+    protected $registry;
 
     public function __construct(
         Configurable $configurable,
-        CollectionFactory $collectionFactory
-    )
-    {
+        CollectionFactory $collectionFactory,
+        Registry $registry
+    ) {
+        $this->registry = $registry;
         $this->configurable = $configurable;
         $this->collectionFactory = $collectionFactory;
     }
 
-    /**
-     * @param Filter $filter
-     * @param AbstractDb $collection
-     * @return bool
-     */
     public function apply(Filter $filter, AbstractDb $collection)
     {
         $conditionType = $filter->getConditionType();
         $attributeName = $filter->getField();
         $attributeValue = $filter->getValue();
+        $category = $this->registry->registry('current_category');
 
-        $virtualAndSimpleProducts = $this->collectionFactory->create()
-            ->addAttributeToFilter($attributeName, $attributeValue, $conditionType)
-            ->getItems();
+        $simpleSelect = $this->collectionFactory->create()
+            ->addAttributeToFilter($attributeName, [$conditionType => $attributeValue])
+            ->addAttributeToFilter('status',
+                \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
+            ->addCategoriesFilter(['in' => (int)$category->getId()]);
 
-        $configurableAndSimpleProductList = [];
+        $simpleSelect->getSelect()
+            ->reset(\Zend_Db_Select::COLUMNS)
+            ->columns(['e.entity_id']);
 
-        /**
-         * @var $product \Magento\Catalog\Model\Product\Interceptor
-         */
-        foreach ($virtualAndSimpleProducts as $product) {
-            $configurableProduct = $this->configurable->getParentIdsByChild($product->getId());
+        $configurableProductsCollection = $this->collectionFactory->create();
+        $select = $configurableProductsCollection->getConnection()
+            ->select()
+            ->distinct()
+            ->from(['l' => 'catalog_product_super_link'], 'l.product_id')
+            ->join(
+                ['k' => $configurableProductsCollection->getTable('catalog_product_entity')],
+                'k.entity_id = l.parent_id'
+            )->where($configurableProductsCollection->getConnection()->prepareSqlCondition(
+                'l.product_id', ['in' => $simpleSelect->getSelect()]
+            ))
+            ->reset(\Zend_Db_Select::COLUMNS)
+            ->columns(['l.parent_id']);
 
-            if (isset($configurableProduct[0])){
-                // use configurable product
-                array_push($configurableAndSimpleProductList, $configurableProduct[0]);
-            } else {
-                // use simple product
-                array_push($configurableAndSimpleProductList, $product->getId());
-            }
-        }
-
-        $configurableAndSimpleProductList = array_unique($configurableAndSimpleProductList);
-
-        $collection->addIdFilter($configurableAndSimpleProductList);
+        $collection->getSelect()->where($collection->getConnection()->prepareSqlCondition(
+            'e.entity_id', ['in' => $select]
+        ));
 
         return true;
     }
