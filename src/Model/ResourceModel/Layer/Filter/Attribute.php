1f3e599919761ad7cd98f04d14640383020651b9
Artjoms Travkovs <artjoms.travkovs@scandiweb.com>
2019-10-24 11:13:44 +0300

    Fixed filters count

---
+++ src/Model/ResourceModel/Layer/Filter/Attribute.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * ScandiPWA_CatalogGraphQl
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_CatalogGraphQl
+ * @author      Kirils Scerba <kirill@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+namespace ScandiPWA\CatalogGraphQl\Model\ResourceModel\Layer\Filter;
+
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Framework\Model\ResourceModel\Db\Context;
+use Magento\Catalog\Model\Layer\Filter\FilterInterface;
+
+/**
+ * Class Attribute
+ * @package ScandiPWA\CatalogGraphQl\Model\ResourceModel\Layer\Filter
+ */
+class Attribute extends \Magento\Catalog\Model\ResourceModel\Layer\Filter\Attribute
+{
+    /**
+     * @var CatalogHelper
+     */
+    protected $catalogHelper;
+
+    /**
+     * @param CatalogHelper $catalogHelper
+     * @param Context $context
+     * @param null $connectionName
+     */
+    public function __construct(
+        Context $context,
+        CatalogHelper $catalogHelper,
+        $connectionName = null
+    )
+    {
+        $this->catalogHelper = $catalogHelper;
+        parent::__construct($context, $connectionName);
+    }
+
+    /**
+     * Retrieve array with products counts per attribute option
+     *
+     * @param FilterInterface $filter
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getCount(FilterInterface $filter)
+    {
+        $category = $this->catalogHelper->getCategory();
+
+        $productCollection = clone $filter->getLayer()->getProductCollection();
+        $productCollection->addCategoriesFilter([ 'in' => (int) $category->getId() ]);
+
+        $select = $productCollection->getSelect();
+        // reset columns, order and limitation conditions
+        $select->reset(\Magento\Framework\DB\Select::COLUMNS);
+        $select->reset(\Magento\Framework\DB\Select::ORDER);
+        $select->reset(\Magento\Framework\DB\Select::LIMIT_COUNT);
+        $select->reset(\Magento\Framework\DB\Select::LIMIT_OFFSET);
+
+        $connection = $this->getConnection();
+        $attribute = $filter->getAttributeModel();
+        $tableAlias = sprintf('%s_idx', $attribute->getAttributeCode());
+        $conditions = [
+            "{$tableAlias}.entity_id = e.entity_id",
+            $connection->quoteInto("{$tableAlias}.attribute_id = ?", $attribute->getAttributeId()),
+            $connection->quoteInto("{$tableAlias}.store_id = ?", $filter->getStoreId()),
+        ];
+
+        $select->join(
+            [$tableAlias => $this->getMainTable()],
+            join(' AND ', $conditions),
+            ['value', 'entity_id']
+        );
+
+        $results = $connection->fetchAll($select);
+
+        return $this->countFilters($results);
+    }
+
+    /**
+     * Counts filters
+     *
+     * @param array $results
+     * @return array
+     */
+    protected function countFilters(array $results): array
+    {
+        $resultArr = [];
+        $existingEntities = [];
+
+        foreach ($results as list('value' => $key, 'entity_id' => $entityId)) {
+            if (!array_key_exists($key, $resultArr)) {
+                $resultArr[$key] = 1;
+                $existingEntities[$key][] = $entityId;
+            } elseif (!in_array($entityId, $existingEntities[$key])) {
+                $resultArr[$key]++;
+                $existingEntities[$key][] = $entityId;
+            }
+        }
+
+        return $resultArr;
+    }
+}
