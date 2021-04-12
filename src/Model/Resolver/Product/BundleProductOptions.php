038108182b30b620423ed5a432a0df3099755b6d
tappiola <tatiana.karamorina@scandiweb.com>
2021-04-12 10:09:50 +0300

    #2479 - calculate bundle price on backend

---
+++ src/Model/Resolver/Product/BundleProductOptions.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace ScandiPWA\CatalogGraphQl\Model\Resolver\Product;
+
+use Magento\Bundle\Model\Product\Price;
+use Magento\Bundle\Model\Product\Type as Bundle;
+use Magento\Catalog\Helper\Data as CatalogData;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Query\EnumLookup;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+/**
+ * @inheritdoc
+ */
+class BundleProductOptions implements ResolverInterface
+{
+    /**
+     * Catalog data
+     *
+     * @var CatalogData
+     */
+    protected $catalogData;
+
+    /**
+     * @var EnumLookup
+     */
+    private $enumLookup;
+
+
+    /**
+     * @param CatalogData $catalogData
+     * @param EnumLookup $enumLookup
+     */
+    public function __construct(
+        EnumLookup $enumLookup,
+        CatalogData $catalogData
+    )
+    {
+        $this->enumLookup = $enumLookup;
+        $this->catalogData = $catalogData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        /** @var \Magento\Bundle\Model\Product\Type $product */
+        $bundleProduct = $value['model'];
+
+        if($bundleProduct->getTypeId() !== Bundle::TYPE_CODE){
+            return [];
+        }
+
+        /** @var \Magento\Bundle\Model\Product\Price $priceModel */
+        $priceModel = $bundleProduct->getPriceModel();
+
+        $result = [];
+
+        /** @var \Magento\Bundle\Model\Option $bundleOption */
+        foreach ($priceModel->getOptions($bundleProduct) as $bundleOption) {
+
+            $selectionsResult = [];
+
+            /* @var \Magento\Bundle\Model\Selection $optionSelection */
+            foreach ($bundleOption->getSelections() as $optionSelection) {
+                // For bundle with fix price taxes are calculated based on the bundle product itself
+                // For bundle with dynamic price taxes are calculated based on the referenced products
+                $taxableItem = $bundleProduct->getPriceType() == Price::PRICE_TYPE_FIXED ? $bundleProduct : $optionSelection;
+
+                $selectionPrice = $priceModel->getSelectionPrice($bundleProduct, $optionSelection, 1);
+                $selectionPriceExclTax = $this->catalogData->getTaxPrice($taxableItem, $selectionPrice, false);
+
+                $selectionPriceType = $this->enumLookup->getEnumValueFromField(
+                    'PriceTypeEnum',
+                    (string) $optionSelection->getSelectionPriceType()
+                ) ?: 'DYNAMIC';
+
+                $regularPrice = $bundleProduct->getPriceType() == Price::PRICE_TYPE_FIXED
+                    ? $selectionPriceType == 'PERCENT'
+                        ? ($bundleProduct->getPrice() * ($optionSelection->getSelectionPriceValue() / 100))
+                        : $optionSelection->getSelectionPriceValue()
+                    : $optionSelection->getPrice();
+
+                $regularPriceExclTax = $this->catalogData->getTaxPrice($taxableItem, $regularPrice, false);
+
+                $selectionsResult[] = [
+                    'selection_id' => $optionSelection->getSelectionId(),
+                    'regular_option_price' => $regularPrice,
+                    'regular_option_price_excl_tax' => $regularPriceExclTax,
+                    'final_option_price' => $selectionPrice,
+                    'final_option_price_excl_tax' => $selectionPriceExclTax
+                ];
+            }
+
+            $result[] = [
+                'option_id' => $bundleOption->getId(),
+                'selection_details' => $selectionsResult
+            ];
+        }
+
+        return $result;
+    }
+}
